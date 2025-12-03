e834a31ed163b99325ae88facb4ea72f76730e38
DixonKH <khaydarovdilshod9@gmail.com>
2025-12-03 16:45:23 +0900

    feat: setup main pages and develop apis in server actions

---
+++ app/actions/action.ts
@@ -0,0 +1,72 @@
+"use-server";
+import { revalidatePath } from "next/cache";
+import { prisma } from "@/lib/prisma";
+import { Todo } from "@prisma/client";
+
+export const getToDos = async (): Promise<Todo[]> => {
+  try {
+    const todos = await prisma.todo.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+    return todos;
+  } catch (error) {
+    console.log("Error fetching todos", error);
+    throw new Error("Error fetching todos");
+  }
+};
+
+export const createToDo = async (title: string, description?: string) => {
+  try {
+    const todo = await prisma.todo.create({
+      data: {
+        title,
+        description: description || "",
+      },
+    });
+    revalidatePath("/");
+    return todo;
+  } catch (error) {
+    console.log("Error creating todo", error);
+    throw new Error("Error creating todo");
+  }
+};
+
+export const updateToDo = async (
+  id: string,
+  title: string,
+  description?: string
+) => {
+  try {
+    const todo = await prisma.todo.update({
+      where: {
+        id,
+      },
+      data: {
+        title,
+        description: description || "",
+      },
+    });
+    revalidatePath("/");
+    return todo;
+  } catch (error) {
+    console.log("Error updating todo", error);
+    throw new Error("Error updating todo");
+  }
+};
+
+export const deleteToDo = async (id: string) => {
+  try {
+    const todo = await prisma.todo.delete({
+      where: {
+        id,
+      },
+    });
+    revalidatePath("/");
+    return todo;
+  } catch (error) {
+    console.log("Error deleting todo", error);
+    throw new Error("Error deleting todo");
+  }
+};
